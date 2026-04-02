b2c013174cf837fcdb076142207b800631979db7
Farstar09 <farstar.work@gmail.com>
2026-04-01 20:13:18 -0700

    Add sitemap and robots for teamrad.gg

---
+++ app/sitemap.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+
+import { getPostMeta } from "@/lib/posts";
+import { getPublicSiteUrl } from "@/lib/env";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const siteUrl = getPublicSiteUrl();
+  const posts = await getPostMeta();
+
+  const staticRoutes = [
+    "",
+    "/about",
+    "/roster",
+    "/staff",
+    "/content",
+    "/partners",
+    "/contact"
+  ];
+
+  const staticEntries: MetadataRoute.Sitemap = staticRoutes.map((route) => ({
+    url: `${siteUrl}${route}`,
+    lastModified: new Date()
+  }));
+
+  const postEntries: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${siteUrl}/content/${post.slug}`,
+    lastModified: post.date ? new Date(post.date) : new Date()
+  }));
+
+  return [...staticEntries, ...postEntries];
+}
